b52c75b6e15e24aa0b60f2ba2a1f22f8e97ab41e
Larry <971877779@qq.com>
2019-11-21 14:17:15 +0800

    2.0.3

---
+++ Sample/app/src/main/java/com/mob/adsdk/sample/DrawAdSettingActivity.java
@@ -0,0 +1,43 @@
+package com.mob.adsdk.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class DrawAdSettingActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "DrawAdSettingActivity";
+
+    private EditText etPosId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_ad_setting);
+        Button btn = findViewById(R.id.loadAd);
+        etPosId = findViewById(R.id.et_pos_id);
+        etPosId.setText(MobConstants.draw_video_id);
+        btn.setText("打开Draw广告");
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loadAd:
+                String posId = etPosId.getText().toString();
+                if (posId.isEmpty()){
+                    posId = MobConstants.draw_video_id;
+                }
+                Intent intent = new Intent(this, DrawNativeVideoActivity.class);
+                intent.putExtra("posId", posId);
+                startActivity(intent);
+                break;
+        }
+    }
+
+
+}
---
+++ Sample/app/src/main/java/com/mob/adsdk/sample/MobConstants.java
@@ -0,0 +1,26 @@
+package com.mob.adsdk.sample;
+
+public class MobConstants {
+
+	public static String splash_id;
+	public static String banner_id;
+	public static String interstitial_id;
+	public static String native_express_id;
+	public static String native_feed_id;
+	public static String reward_id;
+	public static String full_video_id;
+	public static String draw_video_id;
+
+	static {
+
+			splash_id = "1194134766318075906";
+			banner_id = "1193840871388151810";
+			interstitial_id = "1194135018395746306";
+			native_express_id = "1194135313452449794";
+			native_feed_id = "1194135373342916609";
+			reward_id = "1194135203918200833";
+			full_video_id = "1194135154777735170";
+			draw_video_id = "1194135106937503745";
+
+	}
+}
